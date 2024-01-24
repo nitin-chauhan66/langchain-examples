7e0348007279cf82a04918fdbb317ee2a30bd8d9
NITIN CHAUHAN <nvch66@gmail.com>
2024-01-24 15:34:30 +0000

    use langchain-google-genai for gemini pro

--- pinecone-qa/streamlit_app.py
+++ pinecone-qa/streamlit_app.py
@@ -1,21 +1,21 @@
 import os, tempfile
 import streamlit as st
 import pinecone
-from langchain.vectorstores.pinecone import Pinecone
-from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_community.vectorstores.pinecone import Pinecone
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFLoader
-import google.generativeai as genai
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Streamlit app
 st.subheader('Generative Q&A with LangChain & Pinecone (using Sentence Transformers)')
 
 # Get Google API key (if using Cloud TPU) and Pinecone details
 with st.sidebar:
-    google_api_key = st.text_input("Google API key (optional for Cloud TPU)", type="password")  # Add if needed
+    google_api_key = st.text_input("Google API key)", type="password")  # Add if needed
     pinecone_api_key = st.text_input("Pinecone API key", type="password")
-    pinecone_env = st.text_input("Pinecone environment")
-    pinecone_index = st.text_input("Pinecone index name")
+    pinecone_env = st.text_input("Pinecone environment", placeholder="gcp-starter")
+    pinecone_index = st.text_input("Pinecone index name", placeholder="apollo-chatbot")
 source_doc = st.file_uploader("Upload source document", type="pdf", label_visibility="collapsed")
 query = st.text_input("Enter your query")
 
@@ -34,13 +34,11 @@ if st.button("Submit"):
 
             # Generate embeddings using Sentence Transformers, insert into Pinecone, create retriever
             pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
-            embeddings = SentenceTransformerEmbeddings("all-mpnet-base-v2")  # Use a suitable model
+            embeddings = SentenceTransformerEmbeddings()  # Use a suitable model
             vectordb = Pinecone.from_documents(pages, embeddings, index_name=pinecone_index)
             retriever = vectordb.as_retriever()
 
-            genai.configure(api_key=google_api_key)
-
-            model = genai.GenerativeModel('gemini-pro')
+            model = ChatGoogleGenerativeAI(google_api_key=google_api_key, model="gemini-pro", convert_system_message_to_human=True)
             # Initialize the OpenAI module, load and run the Retrieval Q&A chain
             qa = RetrievalQA.from_chain_type(model, chain_type="stuff", retriever=retriever)
             response = qa.run(query)
